43215306ac2263a13ab475c408425ff6bb19a194
Vadim Avkhimenia <avkhimen@ualberta.ca>
2024-01-06 02:30:46 +0000

    deque

--- train_model.py
+++ train_model.py
@@ -1,7 +1,14 @@
 import numpy as np
+from collections import deque
+import random
 
 def main():
-    return
+    de = deque()
+    de.append([1])
+    de.append([2])
+    de.append([3])
+    print(de)
+    print(random.sample(de, 2))
 
 if __name__ == '__main__':
     main()
